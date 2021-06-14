f2474d431a77c80ca3dc4af15de91ca01d4c4b3b
andro <androistuk@gmail.com>
2021-06-14 18:34:19 +0200

    finished responsive for list view, created all drop down menus with jquery and did some styling to elements

--- js/main.js
+++ js/main.js
@@ -16,18 +16,97 @@ tabs.forEach(tab => {
 })
 
 
-/*dropdown menu*/
+/*dropdown menu for LIST items*/
 
-const btn = document.getElementById('btn');
-const menu = document.getElementById('submenu');
+$(document).ready(function() {
+   $('.btn1').click(function() {
+        $('.submenu1').toggleClass('visible');
+    });
+});
 
-menu.style.display = 'none';
+$(document).ready(function() {
+   $('.btn2').click(function() {
+        $('.submenu2').toggleClass('visible');
+    });
+});
 
-btn.addEventListener('click', () =>{
-    if(menu.style.display == 'none'){
-        menu.style.display = 'block';
+$(document).ready(function() {
+   $('.btn3').click(function() {
+        $('.submenu3').toggleClass('visible');
+    });
+});
 
-    } else {
-        menu.style.display = 'none';
-    }
-})
+$(document).ready(function() {
+   $('.btn4').click(function() {
+        $('.submenu4').toggleClass('visible');
+    });
+});
+
+$(document).ready(function() {
+   $('.btn5').click(function() {
+        $('.submenu5').toggleClass('visible');
+    });
+});
+
+$(document).ready(function() {
+   $('.btn6').click(function() {
+        $('.submenu6').toggleClass('visible');
+    });
+});
+
+$(document).ready(function() {
+   $('.btn7').click(function() {
+        $('.submenu7').toggleClass('visible');
+    });
+});
+
+$(document).ready(function() {
+   $('.btn8').click(function() {
+        $('.submenu8').toggleClass('visible');
+    });
+});
+
+/*dropdown menu for GRID items*/
+
+$(document).ready(function() {
+    $('.btn_1').click(function() {
+         $('.submenu_1').toggleClass('show');
+     });
+ });
+
+$(document).ready(function() {
+    $('.btn_2').click(function() {
+         $('.submenu_2').toggleClass('show');
+     });
+ });
+ 
+$(document).ready(function() {
+    $('.btn_3').click(function() {
+         $('.submenu_3').toggleClass('show');
+     });
+ });
+$(document).ready(function() {
+    $('.btn_4').click(function() {
+         $('.submenu_4').toggleClass('show');
+     });
+ });
+$(document).ready(function() {
+    $('.btn_5').click(function() {
+         $('.submenu_5').toggleClass('show');
+     });
+ });
+$(document).ready(function() {
+    $('.btn_6').click(function() {
+         $('.submenu_6').toggleClass('show');
+     });
+ });
+$(document).ready(function() {
+    $('.btn_7').click(function() {
+         $('.submenu_7').toggleClass('show');
+     });
+ });
+$(document).ready(function() {
+    $('.btn_8').click(function() {
+         $('.submenu_8').toggleClass('show');
+     });
+ });
